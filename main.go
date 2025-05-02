1fba484cae24482be88c15368b7009bf106d6d7b
dchan <darren.ccy17@gmail.com>
2025-05-02 23:48:45 +0800

    add cowsay

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	cowsay "github.com/Code-Hex/Neo-cowsay/v2"
+)
 
 func main() {
 	fmt.Println("Hello 🌎")
+	say, err := cowsay.Say(
+		"Howdy 🤠",
+		cowsay.Type("default"),
+		cowsay.BallonWidth(40),
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(say)
 }
